bfc0196f08f8619bd5befa7b26e3840fe869955b
Daniel Mardones <daniel.mardones@github.com>
2025-10-20 19:32:04 -0300

    update cloudflare url

--- frontend/app.py
+++ frontend/app.py
@@ -81,6 +81,7 @@ from urllib.parse import urlparse
 
 # URLs conocidas ordenadas por prioridad (las más recientes primero)
 KNOWN_CF_URLS = [
+    "https://wonder-sufficiently-generator-click.trycloudflare.com",
     "https://respect-craps-lit-aged.trycloudflare.com",
     "https://exchange-ace-inclusive-attraction.trycloudflare.com",
     "https://nodes-avi-twelve-confident.trycloudflare.com",
